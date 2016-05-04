413cb0a98b6db0a63a64c6439770020e34ee3073
melvinzhang <melvinzhang@gmail.com>
2016-05-04 19:22:41 +0800

    extract function for parsing a single line of input

--- src/oracle_parser/core.clj
+++ src/oracle_parser/core.clj
@@ -4,17 +4,19 @@
 
 (def parser (insta/parser "grammar.bnf" :string-ci true))
 
+(defn parse-line [line]
+  (let [parsed (parser line)]
+    (if (insta/failure? parsed)
+      (binding  [*out* *err*]
+        (println "fail :" line)
+        (println "error:" parsed))
+      (do
+        (println "parse:" line)
+        (println "tree :" parsed)))))
+
 (defn parse-file [reader]
   (let [lines (line-seq (java.io.BufferedReader. reader))]
-    (doseq [line lines]
-      (let [parsed (parser line)]
-        (if (insta/failure? parsed)
-          (binding  [*out* *err*]
-            (println "fail :" line)
-            (println "error:" parsed))
-          (do
-            (println "parse:" line)
-            (println "tree :" parsed)))))))
+    (doseq [line lines] (parse-line line))))
 
 (defn -main
   "Parse lines from stdin based on grammar"
