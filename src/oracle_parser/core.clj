f8b26773c9ae022c9a217d330b41eb96dbeeb1ff
melvinzhang <melvinzhang@gmail.com>
2016-05-04 11:46:12 +0800

    extract parsing into parse-file

--- src/oracle_parser/core.clj
+++ src/oracle_parser/core.clj
@@ -2,10 +2,19 @@
   (:require [instaparse.core :as insta])
   (:gen-class))
 
-(def parser (insta/parser "grammar.bnf"))
+(def parser (insta/parser "grammar.bnf" :string-ci true))
+
+(defn parse-file [reader]
+  (let [lines (line-seq (java.io.BufferedReader. reader))]
+    (doseq [line lines]
+      (let [parsed (parser line)]
+        (if (insta/failure? parsed)
+          (println "fail :" line)
+          (do
+            (println "parse:" line)
+            (println "tree :" (parser line))))))))
 
 (defn -main
   "Parse lines from stdin based on grammar"
   [& args]
-  (let [lines (line-seq (java.io.BufferedReader. (clojure.java.io/reader (first args))))]
-    (dorun (map #(do (println "line :" %) (println "parse:"(parser %))) lines))))
+  (parse-file (clojure.java.io/reader (first args))))
