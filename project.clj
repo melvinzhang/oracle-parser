38d4e0a431d1da4a11088aaa10db7c22b058a8b9
melvinzhang <melvinzhang@gmail.com>
2016-05-06 21:35:31 +0800

    extract rules text from AllCards.json file

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.5.1"]
-    [instaparse "1.4.2"]]
+    [instaparse "1.4.2"]
+    [cheshire  "5.6.1"]
+    [org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot oracle-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/oracle_parser/core.clj
+++ src/oracle_parser/core.clj
@@ -1,5 +1,7 @@
 (ns oracle-parser.core
-  (:require [instaparse.core :as insta])
+  (:require [instaparse.core :as insta]
+            [cheshire.core :as json]
+            [clojure.string :as string])
   (:gen-class))
 
 (def parser (insta/parser "grammar.bnf" :string-ci true))
@@ -18,8 +20,25 @@
   (let [lines (line-seq (java.io.BufferedReader. reader))]
     (doseq [line lines] (parse-line line))))
 
+(defn print-rules [file]
+  (let [json (json/parse-string (slurp file))]
+    (doseq [[name data] json]
+      #_(println "==" name "==")
+      (let [text (get data "text" "")
+            parts (-> text
+             (string/replace name "@")
+             (string/replace #" \([^\)]*\)" "")
+             (string/replace "—\n" "— ")
+             (string/replace "\n•" " •")
+             (string/split #"\n"))]
+        (doseq [rule (filter #(> (.length %) 1) parts)]
+          (println rule))))))
+
 (defn -main
   "Parse lines from stdin based on grammar"
   [& args]
-  (with-open [r (clojure.java.io/reader (first args))]
-    (parse-file r)))
+  (let [arg (first args)]
+    (if (.endsWith arg "json")
+      (print-rules arg)
+      (with-open [r (clojure.java.io/reader arg)]
+        (parse-file r)))))
